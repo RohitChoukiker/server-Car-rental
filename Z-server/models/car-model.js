99920daa64f4f51629ea5baf7524c3ac2781353e
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-12-16 05:48:56 +0530

    up

--- Z-server/models/car-model.js
+++ Z-server/models/car-model.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-const { ObjectId } = mongoose.Schema.Types;
+
 
 const carSchema = new mongoose.Schema(
   {
-    owner: { type: ObjectId, ref: "User" },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     brand: { type: String, required: true },
     model: { type: String, required: true },
     image: { type: String },
