8e065b3628321f54ca4e3cc2ec48ea37c6ab2095
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-12-16 05:39:05 +0530

    adding cors in app.js

--- Z-server/app.js
+++ Z-server/app.js
@@ -3,12 +3,19 @@ const app = express();
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 dotenv.config();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use( cors({
+    origin:  process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }));
 
 
 app.use((err, req, res, next) => {
@@ -27,6 +34,7 @@ app.use((err, req, res, next) => {
 
 
 app.use('/api/user', userRoutes);
+app
 
 
 
