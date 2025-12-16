c61680b5af0d67909bc5ef20ae42b3090f23eb4b
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-12-16 05:47:35 +0530

    introduce

--- Z-server/server.js
+++ Z-server/server.js
@@ -9,7 +9,7 @@ const MONGO_URI = process.env.MONGO_URI ;
 mongoose.connect(MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`server is running on port http://localhost:${PORT}`);
     });
 }).catch((err) => {
     console.error('Failed to connect to MongoDB', err);
