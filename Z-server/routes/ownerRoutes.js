4ee2596a11a39cc000309bd8615d9ca249a91d21
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-12-16 13:17:03 +0530

    up

--- Z-server/routes/ownerRoutes.js
+++ Z-server/routes/ownerRoutes.js
@@ -2,25 +2,105 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const Car = require('../models/car-model');
+const restrictedTo = require('../middleware/restrictTo-middleware');
+const portect = require('../middleware/auth-middleware');
 
-router.post('/add-car', catchAsync(async (req, res) => {
-    const
+router.post('/add-car', portect,  restrictedTo("owner"), catchAsync(async (req, res) => {
+//   console.log(req.user._id);
+  const ownerId = req.user._id;
+  const {
+    brand,
+    model,
+    image,
+    year,
+    category,
+    seating_capacity,
+    fuel_type,
+    transmission,
+    pricePerDay,
+    location,
+    description,    
+  } = req.body;
 
+  const car = await Car.create({
+      owner: ownerId,
+      brand,
+      model,
+      year,
+      category,
+      seating_capacity,
+      fuel_type,
+      transmission,
+      pricePerDay,
+      location,
+      description,
+      image: 9,
+      isAvaliable: true,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: 'Car added successfully',
+      data: car,
+    });
 }));
 
 router.post('/toggle-car', catchAsync(async (req, res) => {}));
 
 
-router.get('/get-owner-cars', catchAsync(async (req, res) => {}));
+router.get('/get-owner-cars', portect, restrictedTo("owner"), catchAsync(async (req, res) => {
+  const ownerId = req.user._id;
+  const cars = await Car.find({ owner: ownerId }).populate('owner');
+  
+  res.status(200).json({
+    success: true,
+    results: cars.length,
+    data: cars,
+  });
+}));
 
 
 
 
 
-router.post('/remove-car', catchAsync(async (req, res) => {}));
+router.delete('/remove-car',  portect, restrictedTo("owner"), catchAsync(async (req, res) => {
+  const { carId } = req.body;
+  const ownerId = req.user._id;
+
+  if (!carId) {
+    return res.status(400).json({
+      success: false,
+      message: 'Car ID is required',
+    });
+  }
+
+  const car = await Car.findById(carId);
+
+  if (!car) {
+    return res.status(404).json({
+      success: false,
+      message: 'Car not found',
+    });
+  }
+
+  // Check if the logged-in user is the owner of the car
+  if (car.owner.toString() !== ownerId.toString()) {
+    return res.status(403).json({
+      success: false,
+      message: 'You are not authorized to remove this car',
+    });
+  }
+
+  await Car.findByIdAndDelete(carId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Car removed successfully',
+  });
+}));
 
 
 
-router.post('/update-car', catchAsync(async (req, res) => {}));
+
 
 module.exports = router;
